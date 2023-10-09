333089514146479a667ba555fe6ae882a9c53a83
Star7-jpg <marcosconde612@gmail.com>
2023-10-09 12:29:52 -0600

    01 Manipulación de archivos

---
+++ src/ManipulacionArchivo.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ManipulacionArchivo {
+
+    
+
+    public static void leerArchivo(String name){
+        File archivo; //Apunta a un archivo fisico del dd
+        FileReader reader; // Llave con permiso de solo lectura
+        BufferedReader bufer; //Recuperar info. archivo
+        String linea;
+
+        try{
+            //Crear un apuntador al archivo físico
+            archivo = new File("C:\\archivos\\" + name + ".txt");
+            
+            //Abrir el archivo para lectura
+            reader = new FileReader(archivo);
+
+            //Configurar el bufer para leer datos del archivo
+            bufer = new BufferedReader(reader);
+
+            //Lectura del contenido del archivo
+            while ((linea = bufer.readLine()) != null) {
+                System.out.println("Línea leída: " + linea);
+            }
+            reader.close();
+        }catch(Exception e){
+            System.out.println("Error al leer el archivo: " + e.toString());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String fileName;
+
+        System.out.println("Lectura de un archivo de texto: ");
+        System.out.println("Escribe el nombre del archivo: ");
+        fileName = bufer.readLine();
+
+        leerArchivo(fileName);
+    }
+}
